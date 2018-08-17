4f7f8d3fef59279c5c5909ff09ff5a2920c0c5be
Alix Brunsperger <alix.brunsperger@etraveli.com>
2018-08-17 17:04:27 +0200

    initial commit with images and framework

---
+++ inputs.js
@@ -0,0 +1,44 @@
+/* Inputs */
+
+// Keys pressed
+keys = {
+  left: false,
+  up: false,
+  top: false,
+  attack:false
+};
+
+
+// Key down / keypress (left, top, right)
+onkeydown = onkeypress = function(e){
+  switch(e.keyCode){
+    case 32:
+      keys.attack = true;
+    case 37:
+      keys.left = true;
+      break;
+    case 38:
+      keys.up = true;
+      break;
+    case 39:
+      keys.right = true;
+      break;
+  }
+};
+
+// Key up (left, top, right)
+onkeyup = function(e){
+  switch(e.keyCode){
+    case 32:
+      keys.attack = true;
+    case 37:
+      keys.left = false;
+      break;
+    case 38:
+      keys.up = false;
+      break;
+    case 39:
+      keys.right = false;
+      break;
+  }
+};
